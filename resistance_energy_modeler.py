5ff25374ca769e7fab2eaed3cd9007a319a5f511
Jay Flatland <jayflatland@gmail.com>
2019-03-03 14:46:51 -0600

    Starting to think about effort and watts of pedalling

---
+++ resistance_energy_modeler.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pylab as plt
+import seaborn as sns
+
+# %%
+
+"""
+Model
+10lb force on one pedal
+Pedal lever is about 9 inches long
+
+Rotates pedals from flat to down (90 degrees)
+0% resist - 1.5s (estimated)
+100% resist - 6s (estimated)
+Rotational mass is about ??? (units)
+Assume resistance force is proportional to speed
+
+The torque applied by the weight dissipates as the weight reaches bottom
+The resistance torque is proportional to and in opposition of speed
+
+
+
+"""
