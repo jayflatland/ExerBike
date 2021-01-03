1f4dac17ffc2f04187b9987122924d345575b939
Jay Flatland <jayflatland@gmail.com>
2021-01-03 16:08:02 -0600

    Working

--- exerbike_listener.py
+++ exerbike_listener.py
@@ -46,8 +46,8 @@ pace_log_filenames = [
     #"logs/log_2019-03-04_195042.csv",  # jay
     #"logs/log_2019-03-05_193429.csv",  # jay
     #"logs/log_2019-03-06_202052.csv",  # jay
-    "logs/log_2019-03-07_202639.csv",  # jay
-    "logs/log_2019-08-20_095304.csv",  # jay
+    #"logs/log_2019-03-07_202639.csv",  # jay
+    #"logs/log_2019-08-20_095304.csv",  # jay
     #"logs/log_2019-02-27_202229.csv",  # caleb
     #"logs/log_2019-02-28_202446.csv",  # caleb
 ]
@@ -59,22 +59,24 @@ for fname in pace_log_filenames:
     last_paces.append(last_pace)
 
 t_vals = []
-hb_vals = []
+#hb_vals = []
 pedal_vals = []
 resist_pct_vals = []
-hb_bpm_vals = []
+#hb_bpm_vals = []
 tgt_resist_pct_vals = []
+power_vals = []
+work_vals = []
 
 log_file = f'logs/log_{time.strftime("%Y-%m-%d_%H%M%S")}.csv'
 log_fd = open(log_file, 'w')
-print("t,hb_cnt,pedal_cnt,resist_pct,hb_bpm", file=log_fd)
+print("t,pedal_cnt,resist_pct,tgt_resist_pct,power,work", file=log_fd)
 
-ref_pace = pd.DataFrame({
-    "t": np.arange(0, 1200.0, 1.0),
-})
-ref_pace['pedal_cnt'] = 1000.0 / 1200.0
-ref_pace['resist_pct'] = 0.3
-ref_pace['hb_bpm'] = 125.0
+# ref_pace = pd.DataFrame({
+#     "t": np.arange(0, 1200.0, 1.0),
+# })
+# ref_pace['pedal_cnt'] = 1000.0 / 1200.0
+# ref_pace['resist_pct'] = 0.3
+# ref_pace['hb_bpm'] = 125.0
 
 def animate(i):
     try:
@@ -88,44 +90,50 @@ def animate(i):
 
     t = time.time()
     pedal_cnt = int(parts[0])
-    hb_cnt = int(parts[1])
-    resist_pct = float(parts[2])
-    hb_bpm = float(parts[3])
-    tgt_resist_pct = float(parts[4])
+    #hb_cnt = int(parts[1])
+    resist_pct = float(parts[1])
+    #hb_bpm = float(parts[3])
+    tgt_resist_pct = float(parts[2])
+    power = float(parts[3])
+    work = float(parts[4])
 
     t_vals.append(t)
-    hb_vals.append(hb_cnt)
+    #hb_vals.append(hb_cnt)
     pedal_vals.append(pedal_cnt)
     resist_pct_vals.append(resist_pct)
-    hb_bpm_vals.append(hb_bpm)
+    #hb_bpm_vals.append(hb_bpm)
     tgt_resist_pct_vals.append(tgt_resist_pct)
+    power_vals.append(power)
+    work_vals.append(work)
 
     df = pd.DataFrame({
         "t": t_vals,
-        "hb_cnt": hb_vals,
+        #"hb_cnt": hb_vals,
         "pedal_cnt": pedal_vals,
         "resist_pct": resist_pct_vals,
-        "hb_bpm": hb_bpm_vals,
+        #"hb_bpm": hb_bpm_vals,
         "tgt_resist_pct": tgt_resist_pct_vals,
+        "power": power_vals,
+        "work": work_vals,
     })
     df['t'] = df['t'] - df['t'].min()
 
     dfs = [
-        (ref_pace,  1.0, "blue"),
+        # (ref_pace,  1.0, "blue"),
         (df,        1.0, "red"),
     ]
 
-    for d in last_paces:
-        dfs.append((d, 0.2, "black"))
+    # for d in last_paces:
+    #     dfs.append((d, 0.2, "black"))
 
-    for d, alph, clr in dfs:
-        wndsz = 60
-        d['pedal'] = d['pedal_cnt'].cumsum()
-        d['pedal_rpm'] = (d['pedal'] - d['pedal'].rolling(wndsz, min_periods=1).min()) * 60.0 / wndsz
-        d['pedal_rate'] = d['pedal_rpm'] / 60.0 * math.pi * 2.0
-        d['resist_torque_per_vel'] = 1.8 + 9.0 * d['resist_pct'].rolling(wndsz, min_periods=1).mean()
-        d['power'] = d['pedal_rate'] * d['pedal_rate'] * d['resist_torque_per_vel']
-        d['work'] = (d['power'] * (d['t'] - d['t'].shift(1))).cumsum()
+    # for d, alph, clr in dfs:
+    #     wndsz = 60
+    #     d['pedal'] = d['pedal_cnt'].cumsum()
+    #     d['pedal_rpm'] = (d['pedal'] - d['pedal'].rolling(wndsz, min_periods=1).min()) * 60.0 / wndsz
+    #     d['pedal_rate'] = d['pedal_rpm'] / 60.0 * math.pi * 2.0
+    #     d['resist_torque_per_vel'] = 1.8 + 9.0 * d['resist_pct'].rolling(wndsz, min_periods=1).mean()
+    #     d['power'] = d['pedal_rate'] * d['pedal_rate'] * d['resist_torque_per_vel']
+    #     d['work'] = (d['power'] * (d['t'] - d['t'].shift(1))).cumsum()
 
 
     ax = axs[0][0]
@@ -137,10 +145,10 @@ def animate(i):
 
     ax = axs[0][1]
     ax.clear()
-    ax.set_title(f"Pedal Count: {df.pedal_cnt.sum()}")
+    ax.set_title(f"Pedal Count: {df.pedal_cnt.values[-1]}")
     for d, alph, clr in dfs:
         d = d.copy()
-        d['pedal_cnt'] = d['pedal_cnt'].cumsum()
+        d['pedal_cnt'] = d['pedal_cnt']
         d = d[d.t > df.t.max() - 120.0]
         d = d[d.t < df.t.max()]
         ax.plot(d.t, d.pedal_cnt, alpha=alph, c=clr)
@@ -151,13 +159,6 @@ def animate(i):
     for d, alph, clr in dfs:
         d = d[d.t > df.t.min()]
         ax.plot(d.t, d.pedal_cnt.cumsum(), alpha=alph, c=clr)
-
-    ax = axs[1][1]
-    ax.clear()
-    ax.set_title(f"Heart: {hb_bpm} bpm")
-    for d, alph, clr in dfs:
-        ax.plot(d.t, d.hb_bpm, alpha=alph, c=clr)
-    ax.set_ylim(30.0, 200.0)
 
     ax = axs[2][0]
     ax.clear()
@@ -171,7 +172,8 @@ def animate(i):
     for d, alph, clr in dfs:
         ax.plot(d.t, d.work / 4184.0 / gme, alpha=alph, c=clr)
 
-    print(f"{t},{hb_cnt},{pedal_cnt},{resist_pct},{hb_bpm}", file=log_fd)
+    #print(f"{t},{hb_cnt},{pedal_cnt},{resist_pct},{hb_bpm}", file=log_fd)
+    print(f"{t},{pedal_cnt},{resist_pct},{tgt_resist_pct},{power},{work}", file=log_fd)
     log_fd.flush()
 ani = animation.FuncAnimation(fig, animate, interval=100)
 plt.tight_layout()
--- sketch_aug26a/sketch_aug26a.ino
+++ sketch_aug26a/sketch_aug26a.ino
@@ -1,7 +1,7 @@
 #include <WiFi.h>
 #include <WiFiUdp.h>
 
-const char * networkName = "jaysplace2";
+const char * networkName = "To The Oasis";
 const char * networkPswd = "deadbeef";
 
 WiFiUDP Udp;
@@ -17,6 +17,11 @@ int motor2Pin = 22;
 int resistKnobPin = 35;
 
 #define HBUF_SZ 256
+#define GME 0.2 //gross mechanical efficiency
+
+#define CAL(joules) ((joules) / 4.184 / GME)
+#define KCAL(joules) ((joules) / 4184.0 / GME)
+
 float heartHist[HBUF_SZ];
 int heartIdx = 0;
 
@@ -30,11 +35,14 @@ float heartBPM = 0.0;
 float lastSpeedo = 0.0;
 
 long lastReportMillis = 0;
+long lastPedalCount = -1;
 
 long loopDtMs = 5;//careful changing this - DSP filters below are tuned to this
 
 long pedalCount = 0;
 long hbCount = 0;
+long lastPedalCountedTime = -1;
+
 
 void connectToWiFi(const char * ssid, const char * pwd)
 {
@@ -74,6 +82,8 @@ float heart = 0.0;
 long last_hb_millis = 0;
 float hb_bpm = 0.0;
 float last_heart_pulse = 0.0;
+float totalWork = 0.0;
+float power = 0.0;
 
 void loop()
 {
@@ -95,10 +105,26 @@ void loop()
         hbCount = 0;
     }
 
+    float timeForThisPedal = (now - lastPedalCountedTime) * 0.001;
     if(lastSpeedo > 0.7 && speedo < 0.3) {
         pedalCount++;
+        if(timeForThisPedal < 10.0) {
+            float pedal_rpm = 60.0 / timeForThisPedal;
+            float pedal_rate = pedal_rpm / 60.0 * PI * 2.0;
+            float resist_torque_per_vel = 1.8 + 9.0 * resisto;
+            power = pedal_rate * pedal_rate * resist_torque_per_vel;
+            float work = power * timeForThisPedal;
+            totalWork += work;
+        }
+
+        lastPedalCountedTime = now;
     }
     lastSpeedo = speedo;
+
+    //if idle for 10 minutes, restart
+    if(timeForThisPedal > 60.0) {
+        totalWork = 0.0;
+    }
 
     ///////////////////////////////////////////////////////////////////////////
     // Heart filtering
@@ -168,7 +194,7 @@ void loop()
     ///////////////////////////////////////////////////////////////////////////
     // REPORTING
     ///////////////////////////////////////////////////////////////////////////
-    if(1) {  // raw heart signal diagnostics
+    if(0) {  // raw heart signal diagnostics
         if(now - lastReportMillis > 0) {
             lastReportMillis = now;
             Serial.println(
@@ -182,21 +208,269 @@ void loop()
     }
 
     else {
-        if(now - lastReportMillis > 1000) {
+        //if(now - lastReportMillis > 1000) {
+
+        // d['pedal'] = d['pedal_cnt'].cumsum()
+        // d['pedal_rpm'] = (d['pedal'] - d['pedal'].rolling(wndsz, min_periods=1).min()) * 60.0 / wndsz
+        // d['pedal_rate'] = d['pedal_rpm'] / 60.0 * math.pi * 2.0
+        // d['resist_torque_per_vel'] = 1.8 + 9.0 * d['resist_pct'].rolling(wndsz, min_periods=1).mean()
+        // d['power'] = d['pedal_rate'] * d['pedal_rate'] * d['resist_torque_per_vel']
+        // d['work'] = (d['power'] * (d['t'] - d['t'].shift(1))).cumsum()
+
+        if(pedalCount != lastPedalCount || now - lastReportMillis > 10000) {
             lastReportMillis = now;
+            lastPedalCount = pedalCount;
 
             String msg = String(pedalCount) + "," +
-                         String(hbCount) + "," +
                          String(resisto) + "," +
-                         String(hb_bpm) + "," + 
-                         String(targetResist);
-            Udp.beginPacket("10.1.10.255", 10245);
+                         String(targetResist) + "," + 
+                         String(power) + "," + 
+                         String(totalWork);
+            Serial.println(msg);
+            msg = msg + "\n";
+            //Udp.beginPacket("10.1.10.255", 10245);
+            Udp.beginPacket("10.1.10.12", 10245);
             Udp.write((const uint8_t*)msg.c_str(), msg.length());
             Udp.endPacket();
 
-            pedalCount = 0;
+            // pedalCount = 0;
             hbCount = 0;
         }
     }
 
 }
+
+
+
+
+
+// /*********
+//   Rui Santos
+//   Complete project details at https://RandomNerdTutorials.com/esp32-websocket-server-arduino/
+//   The above copyright notice and this permission notice shall be included in all
+//   copies or substantial portions of the Software.
+// *********/
+
+// // Import required libraries
+// #include <WiFi.h>
+// #include <AsyncTCP.h>
+// #include <ESPAsyncWebServer.h>
+
+// // Replace with your network credentials
+// const char* ssid = "To The Oasis";
+// const char* password = "deadbeef";
+
+// bool ledState = 0;
+// const int ledPin = 2;
+
+// // Create AsyncWebServer object on port 80
+// AsyncWebServer server(80);
+// AsyncWebSocket ws("/ws");
+
+// const char index_html[] PROGMEM = R"rawliteral(
+// <!DOCTYPE HTML><html>
+// <head>
+//   <title>ESP Web Server</title>
+//   <meta name="viewport" content="width=device-width, initial-scale=1">
+//   <link rel="icon" href="data:,">
+//   <style>
+//   html {
+//     font-family: Arial, Helvetica, sans-serif;
+//     text-align: center;
+//   }
+//   h1 {
+//     font-size: 1.8rem;
+//     color: white;
+//   }
+//   h2{
+//     font-size: 1.5rem;
+//     font-weight: bold;
+//     color: #143642;
+//   }
+//   .topnav {
+//     overflow: hidden;
+//     background-color: #143642;
+//   }
+//   body {
+//     margin: 0;
+//   }
+//   .content {
+//     padding: 30px;
+//     max-width: 600px;
+//     margin: 0 auto;
+//   }
+//   .card {
+//     background-color: #F8F7F9;;
+//     box-shadow: 2px 2px 12px 1px rgba(140,140,140,.5);
+//     padding-top:10px;
+//     padding-bottom:20px;
+//   }
+//   .button {
+//     padding: 15px 50px;
+//     font-size: 24px;
+//     text-align: center;
+//     outline: none;
+//     color: #fff;
+//     background-color: #0f8b8d;
+//     border: none;
+//     border-radius: 5px;
+//     -webkit-touch-callout: none;
+//     -webkit-user-select: none;
+//     -khtml-user-select: none;
+//     -moz-user-select: none;
+//     -ms-user-select: none;
+//     user-select: none;
+//     -webkit-tap-highlight-color: rgba(0,0,0,0);
+//    }
+//    /*.button:hover {background-color: #0f8b8d}*/
+//    .button:active {
+//      background-color: #0f8b8d;
+//      box-shadow: 2 2px #CDCDCD;
+//      transform: translateY(2px);
+//    }
+//    .state {
+//      font-size: 1.5rem;
+//      color:#8c8c8c;
+//      font-weight: bold;
+//    }
+//   </style>
+// <title>ESP Web Server</title>
+// <meta name="viewport" content="width=device-width, initial-scale=1">
+// <link rel="icon" href="data:,">
+// </head>
+// <body>
+//   <div class="topnav">
+//     <h1>ESP WebSocket Server</h1>
+//   </div>
+//   <div class="content">
+//     <div class="card">
+//       <h2>Output - GPIO 2</h2>
+//       <p class="state">state: <span id="state">%STATE%</span></p>
+//       <p><button id="button" class="button">Toggle</button></p>
+//     </div>
+//   </div>
+// <script>
+//   var gateway = `ws://${window.location.hostname}/ws`;
+//   var websocket;
+//   window.addEventListener('load', onLoad);
+//   function initWebSocket() {
+//     console.log('Trying to open a WebSocket connection...');
+//     websocket = new WebSocket(gateway);
+//     websocket.onopen    = onOpen;
+//     websocket.onclose   = onClose;
+//     websocket.onmessage = onMessage; // <-- add this line
+//   }
+//   function onOpen(event) {
+//     console.log('Connection opened');
+//   }
+//   function onClose(event) {
+//     console.log('Connection closed');
+//     setTimeout(initWebSocket, 2000);
+//   }
+//   function onMessage(event) {
+//     var state;
+//     if (event.data == "1"){
+//       state = "ON";
+//     }
+//     else{
+//       state = "OFF";
+//     }
+//     document.getElementById('state').innerHTML = state;
+//   }
+//   function onLoad(event) {
+//     initWebSocket();
+//     initButton();
+//   }
+//   function initButton() {
+//     document.getElementById('button').addEventListener('click', toggle);
+//   }
+//   function toggle(){
+//     websocket.send('toggle');
+//   }
+// </script>
+// </body>
+// </html>
+// )rawliteral";
+
+// void notifyClients() {
+//   ws.textAll(String(ledState));
+// }
+
+// void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
+//   AwsFrameInfo *info = (AwsFrameInfo*)arg;
+//   if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
+//     data[len] = 0;
+//     if (strcmp((char*)data, "toggle") == 0) {
+//       ledState = !ledState;
+//       notifyClients();
+//     }
+//   }
+// }
+
+// void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type,
+//              void *arg, uint8_t *data, size_t len) {
+//   switch (type) {
+//     case WS_EVT_CONNECT:
+//       Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+//       break;
+//     case WS_EVT_DISCONNECT:
+//       Serial.printf("WebSocket client #%u disconnected\n", client->id());
+//       break;
+//     case WS_EVT_DATA:
+//       handleWebSocketMessage(arg, data, len);
+//       break;
+//     case WS_EVT_PONG:
+//     case WS_EVT_ERROR:
+//       break;
+//   }
+// }
+
+// void initWebSocket() {
+//   ws.onEvent(onEvent);
+//   server.addHandler(&ws);
+// }
+
+// String processor(const String& var){
+//   Serial.println(var);
+//   if(var == "STATE"){
+//     if (ledState){
+//       return "ON";
+//     }
+//     else{
+//       return "OFF";
+//     }
+//   }
+// }
+
+// void setup(){
+//   // Serial port for debugging purposes
+//   Serial.begin(115200);
+
+//   pinMode(ledPin, OUTPUT);
+//   digitalWrite(ledPin, LOW);
+  
+//   // Connect to Wi-Fi
+//   WiFi.begin(ssid, password);
+//   while (WiFi.status() != WL_CONNECTED) {
+//     delay(1000);
+//     Serial.println("Connecting to WiFi..");
+//   }
+
+//   // Print ESP Local IP Address
+//   Serial.println(WiFi.localIP());
+
+//   initWebSocket();
+
+//   // Route for root / web page
+//   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+//     request->send_P(200, "text/html", index_html, processor);
+//   });
+
+//   // Start server
+//   server.begin();
+// }
+
+// void loop() {
+//   ws.cleanupClients();
+//   digitalWrite(ledPin, ledState);
+// }
