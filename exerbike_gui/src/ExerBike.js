de8ca138792cad3a116c964ce989a550e6333ceb
Jay Flatland <jayflatland@gmail.com>
2023-02-03 06:42:28 -0600

    Tinkering

--- exerbike_gui/src/ExerBike.js
+++ exerbike_gui/src/ExerBike.js
@@ -1,25 +1,45 @@
-import React, { useState, useCallback, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import useWebSocket, { ReadyState } from 'react-use-websocket';
 import C3Chart from 'react-c3js';
 import 'c3/c3.css';
 
-export default function ExerBike() {
-    const [socketUrl, setSocketUrl] = useState('ws://10.1.10.178/exerbike');
-    const [totalKCalHistory, setTotalKCalHistory] = useState([]);
-    const [totalKCal, setTotalKCal] = useState(0.0);
+export default function ExerBike(props) {
+    const [socketUrl] = useState('ws://10.1.10.177/exerbike');
+    const [state, setState] = useState({
+        "times": [],
+        "kcals": [],
+        "totalKCals": [],
+        "totalKCal": 0.0,
+        "startTime": undefined
+    });
 
     const { lastMessage, readyState } = useWebSocket(socketUrl);
+
+    const canvasRef = useRef(null)
+    // const canvas = canvasRef.current
+    // const context = canvas.getContext('2d')
 
     useEffect(() => {
         if (lastMessage !== null) {
             var d = JSON.parse(lastMessage.data);
             console.log(d);
-            var newTotalKCal = totalKCal + d.work_kcal;
-            setTotalKCal(newTotalKCal);
-            setTotalKCalHistory((prev) => prev.concat(newTotalKCal));
-            // var samples = ['data'].concat(d.samples);
-            // setData({columns: [samples]});
+
+            var now = Date.now();
+            setState((prevState) => { return {
+                "times": prevState.times.concat(now),
+                "kcals": prevState.kcals.concat(d.work_kcal),
+                "totalKCals": prevState.kcals.concat(prevState.totalKCal + d.work_kcal),
+                "totalKCal": prevState.totalKCal + d.work_kcal,
+                "startTime": prevState.startTime === undefined ? now : prevState.startTime
+            }; });
         }
+
+        const canvas = canvasRef.current
+        const context = canvas.getContext('2d')
+        //Our first draw
+        context.fillStyle = '#0000ff';
+        context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+  
     }, [lastMessage]);
 
     const connectionStatus = {
@@ -33,8 +53,15 @@ export default function ExerBike() {
     return (
         <div>
             <span>The WebSocket is currently {connectionStatus}</span>
-            <h1>You Have Burned {totalKCal.toFixed(2)} Calories!</h1>
-            <C3Chart data={{columns: [['data', ...totalKCalHistory]]}} />
+            <h1>You Have Burned {state.totalKCal.toFixed(2)} Calories!</h1>
+            <C3Chart data={{
+                x: 'x',
+                columns: [
+                    ['x', ...state.times],
+                    ['kcal', ...state.kcals]
+                ],
+            }} />
+            <canvas ref={canvasRef} {...props}/>
         </div>
     );
 };
